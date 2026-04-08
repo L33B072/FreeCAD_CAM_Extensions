c69b0c6916756ddc8c32c5022caafd84d2533675
Lee Bernard <lee.bernard@mogpm.com>
2026-04-08 11:06:15 -0500

    Refactor: Integrate as CAM workbench extension instead of separate workbench

--- InitGui.py
+++ InitGui.py
@@ -12,68 +12,53 @@ import FreeCAD
 import FreeCADGui
 
 
-class CAMExtensionsWorkbench(FreeCADGui.Workbench):
-    """CAM Extensions workbench"""
-    
-    MenuText = "CAM Extensions"
-    ToolTip = "Enhanced CAM workbench features"
-    Icon = """
-        /* XPM */
-        static char * cam_ext_xpm[] = {
-        "16 16 3 1",
-        " 	c None",
-        ".	c #000000",
-        "+	c #0000FF",
-        "                ",
-        "   .........    ",
-        "   .+++++++.    ",
-        "   .+.....+.    ",
-        "   .+.+++.+.    ",
-        "   .+.+.+.+.    ",
-        "   .+.+++.+.    ",
-        "   .+.....+.    ",
-        "   .+++++++.    ",
-        "   .++...++.    ",
-        "   .+.+++.+.    ",
-        "   .+.+.+.+.    ",
-        "   .+.+++.+.    ",
-        "   .++...++.    ",
-        "   .........    ",
-        "                "};
-        """
-    
-    def Initialize(self):
-        """This function is executed when FreeCAD starts"""
-        import OperationVariablesPanel
-        
-        # Import the command classes
-        import CAMExtensions_Commands
-        
-        # List of command names
-        self.list = ["CAM_ShowOperationVariables"]
-        
-        # Create toolbars and menus
-        self.appendToolbar("CAM Extensions", self.list)
-        self.appendMenu("CAM E&xtensions", self.list)
-        
-        FreeCAD.Console.PrintMessage("CAM Extensions workbench initialized\n")
-    
-    def Activated(self):
-        """This function is executed when the workbench is activated"""
-        return
-    
-    def Deactivated(self):
-        """This function is executed when the workbench is deactivated"""
-        return
-    
-    def ContextMenu(self, recipient):
-        """This is executed whenever the user right-clicks on screen"""
-        self.appendContextMenu("CAM Extensions", self.list)
-    
-    def GetClassName(self):
-        """Return workbench classname"""
-        return "Gui::PythonWorkbench"
+# Import and register commands immediately (available in all workbenches)
+import CAMExtensions_Commands
+
+FreeCAD.Console.PrintMessage("CAM Extensions loaded - commands available in CAM workbench\n")
 
 
-# Add the workbench to FreeCAD
-FreeCADGui.addWorkbench(CAMExtensionsWorkbench())
+# Extend the CAM workbench to add our commands
+class CAMWorkbenchExtension:
+    """Extension to add commands to the CAM workbench"""
+    
+    def __init__(self):
+        # Get the original CAM workbench
+        self.cam_wb = FreeCADGui.getWorkbench("PathWorkbench")
+        if self.cam_wb:
+            self.extend_cam_workbench()
+    
+    def extend_cam_workbench(self):
+        """Add our commands to the CAM workbench menus and toolbars"""
+        try:
+            # Store the original Initialize method
+            original_initialize = self.cam_wb.Initialize
+            
+            # Define new Initialize that calls original + adds our stuff
+            def new_initialize():
+                original_initialize()
+                
+                # Add our commands to CAM workbench
+                cam_extensions_list = ["CAM_ShowOperationVariables"]
+                
+                # Add to a submenu in the CAM menu
+                self.cam_wb.appendMenu(["CAM", "E&xtensions"], cam_extensions_list)
+                
+                # Optionally add to toolbar
+                self.cam_wb.appendToolbar("CAM Extensions", cam_extensions_list)
+                
+                FreeCAD.Console.PrintMessage("CAM Extensions commands added to CAM workbench\n")
+            
+            # Replace the Initialize method
+            self.cam_wb.Initialize = new_initialize
+            
+        except Exception as e:
+            FreeCAD.Console.PrintWarning(f"Could not extend CAM workbench: {e}\n")
+            FreeCAD.Console.PrintWarning("Commands are still available via Python console\n")
+
+
+# Try to extend the CAM workbench
+try:
+    extension = CAMWorkbenchExtension()
+except Exception as e:
+    FreeCAD.Console.PrintWarning(f"CAM Extensions: {e}\n")
